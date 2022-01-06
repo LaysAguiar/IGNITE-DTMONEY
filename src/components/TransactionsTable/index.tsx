05b4b5211006ff26e9a10ef5e33829cd5659fdf3
Lays Aguiar <lays.vidal.de.aguiar@outlook.com>
2022-01-06 14:51:36 -0300

    first commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { Container } from "./style";
+
+export function TransactionsTable() {
+     return (
+          <Container>
+               <table>
+                    <thead>
+                         <tr>
+                              <th>Titulo</th>
+                              <th>Valor</th>
+                              <th>Categoria</th>
+                              <th>Data</th>
+                         </tr>
+                    </thead>
+                    <tbody>
+                         <tr>
+                              <td>Desenvolvimento</td>
+                              <td className="deposit">R$ 12.000</td>
+                              <td>Desenvolvimento</td>
+                              <td>20/10/2021</td>
+                         </tr>
+                    </tbody>
+                    <tbody>
+                         <tr>
+                              <td>Aluguel</td>
+                              <td className="wihtdraw">- R$ 1000,00</td>
+                              <td>Habitação</td>
+                              <td>20/10/2021</td>
+                         </tr>
+                    </tbody>
+
+               </table>
+
+          </Container>
+
+     )
+
+}
